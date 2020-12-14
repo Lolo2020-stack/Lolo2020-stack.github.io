043f4f49c4e30eb9d60378a751bf28a3280107ab
Lolo2020-stack <68825853+Lolo2020-stack@users.noreply.github.com>
2020-12-14 10:47:48 -0800

    Update sayhello.js

--- Module4-solution/sayhello.js
+++ Module4-solution/sayhello.js
@@ -1,13 +1,10 @@
-(function(){
- var helloSpeaker = {};
- 
-
-var speakWord = "Hello";
-helloSpeaker.speak = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-
+(function(window){
+  var speakWord = "Hello";
+  var helloSpeaker = {
+     speak: function (name){
+       console.log(speakWord + " " + name);
+  }
+};
 window.helloSpeaker = helloSpeaker;
-
-})();
+})(window);
 
